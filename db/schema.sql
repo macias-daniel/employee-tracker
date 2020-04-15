ccbb0f62e7e1a673de346aae3685ba2c422a5c1b
PinkFished <d.macias.apps@gmail.com>
2020-04-14 23:34:05 -0700

    Added file structure and db schema

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+CREATE TABLE employee (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+    
+	first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER, 
+    
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+    
+	name VARCHAR(50) NOT NULL,
+    
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+    
+	title VARCHAR(50) NOT NULL,
+    salary DECIMAL (10, 3) NOT NULL,
+    
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM employee;
+SELECT * FROM role;
+SELECT * FROM department;
